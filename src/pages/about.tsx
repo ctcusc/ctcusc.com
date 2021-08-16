f8971de0d142efcc0852ecb80f86379af35e1913
Bryan Huang <bryanhua@usc.edu>
2021-08-16 02:04:48 -0400

    landing more fun

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,41 +4,11 @@ import Image from 'next/image'
 import Team from '../components/team'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import Value from '../components/value'
 import aboutStyles from '../styles/about.module.css'
 import sharedStyles from '../styles/shared.module.css'
-import group from '../../public/2017-CTC-Group.jpg'
-import retreat from '../../public/gopro-retreat.jpg'
-import dinner from '../../public/ctc-dinner.jpg'
 import great from '../../public/placement1.svg'
 import things from '../../public/placement2.svg'
-
-const values = [
-  {
-    number: 1,
-    title: 'Impact',
-    desc:
-      'Do you care about creating a positive impact in your communities? This might be through projects, active volunteering, or advocacy.',
-  },
-  {
-    number: 2,
-    title: 'Community',
-    desc:
-      'How do you collaborate with others? We value diverse, new opinions and extend this respect beyond our projects and into friendships.',
-  },
-  {
-    number: 3,
-    title: 'Growth',
-    desc:
-      'Do you exhibit a desire for continuous growth? Onboarding, mentorship, learning from others are huge parts of our organization.',
-  },
-  {
-    number: 4,
-    title: 'Initiative',
-    desc:
-      'Are you self-motivated to call out injustices and tackle new problems? CTC thrives under ambiguous "no-right-answer" challenges.',
-  },
-]
+import { motion } from 'framer-motion'
 
 const news = [
   {
@@ -53,41 +23,46 @@ const Index = () => {
     <div className={sharedStyles.layout}>
       <Header titlePre="About" />
       <h1>About us.</h1>
-      <div className={aboutStyles.photogrid}>
-        <div className={aboutStyles.photo}>
-          <Image
-            src={group}
-            alt="All Team Photo of Code the Change"
-            placeholder="blur"
-          />
-        </div>
-        <Image
-          className={aboutStyles.photo}
-          src={dinner}
-          alt="Code the Change Dinner Photo"
-          placeholder="blur"
-        />
-        <Image
-          className={aboutStyles.photo}
-          src={retreat}
-          alt="Code the Change at Retreat"
-          placeholder="blur"
-        />
+      <div className={aboutStyles.explanation}>
+        <p>Welcome!</p>
+        <p>
+          Code the Change started in 2015 by a group of civic-minded developers
+          and designers looking to use their skills for service.
+        </p>
+        <p>
+          Since then, we’ve partnered with around three nonprofits yearly,
+          working in solidarity with the communities to help our partner
+          organizations accomplish their missions.
+        </p>
+        <p>
+          However, it goes beyond the vague buzzword of “tech for social good”.
+          We as technologists have a responsibility to hold our community
+          accountable for tech’s harms and its role in society.
+        </p>
+        <p>
+          We must do better than technosolutionism—we’re currently growing our
+          organization not just to build short term projects, but engage our
+          members in activism, foster discussions on campus, and promote service
+          in our communities.
+        </p>
+        <p>
+          If this mission toward a better future resonates, we’d love to have
+          you join us.
+        </p>
       </div>
-      <div className={aboutStyles.mission}>
-        <div>
-          <h5>OUR MISSION</h5>
-          <h3>
-            Design and build sustainable software for nonprofit organizations to
-            create social change.
-          </h3>
-        </div>
-      </div>
-      <h2>Our Values</h2>
-      <div className={aboutStyles.values}>
-        {values.map((value) => {
-          return <Value {...value} />
-        })}
+      <div className={aboutStyles.buttons}>
+        <Link href="/students/">
+          <motion.div
+            whileHover={{
+              y: 4,
+              transition: { duration: 0.125 },
+            }}
+            whileTap={{ scale: 0.95 }}
+            className={aboutStyles.primarybutton}
+          >
+            Apply now
+          </motion.div>
+        </Link>
       </div>
       <h1>Our Team</h1>
       <p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,41 @@ import Features from '../components/features'
 import landingStyles from '../styles/landing.module.css'
 import sharedStyles from '../styles/shared.module.css'
 import Image from 'next/image'
-import { motion, useAnimation } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react'
 import organization from '../../public/alumni_pic.jpg'
+import Value from '../components/value'
+import group from '../../public/2017-CTC-Group.jpg'
+import retreat from '../../public/gopro-retreat.jpg'
+import dinner from '../../public/ctc-dinner.jpg'
+
+const values = [
+  {
+    number: 1,
+    title: 'Impact',
+    desc:
+      'Do you care about creating a positive impact in your communities? This might be through projects, active volunteering, or advocacy.',
+  },
+  {
+    number: 2,
+    title: 'Community',
+    desc:
+      'How do you collaborate with others? We value diverse, new opinions and extend this respect beyond our projects and into friendships.',
+  },
+  {
+    number: 3,
+    title: 'Growth',
+    desc:
+      'Do you exhibit a desire for continuous growth? Onboarding, mentorship, learning from others are huge parts of our organization.',
+  },
+  {
+    number: 4,
+    title: 'Initiative',
+    desc:
+      'Are you self-motivated to call out injustices and tackle new problems? CTC thrives under ambiguous "no-right-answer" challenges.',
+  },
+]
 
 export default function Index() {
   return (
@@ -54,50 +85,64 @@ export default function Index() {
             </Link>
           </div>
         </div>
-        <div></div>
-        <div className={landingStyles.explanation}>
-          <p>Welcome!</p>
-          <p>
-            Code the Change started in 2015 by a group of civic-minded
-            developers and designers looking to use their skills for service.
-          </p>
-          <p>
-            Since then, we’ve partnered with around three nonprofits yearly,
-            working in solidarity with the communities to help our partner
-            organizations accomplish their missions.
-          </p>
-          <p>
-            However, it goes beyond the vague buzzword of “tech for social
-            good”. We as technologists have a responsibility to hold our
-            community accountable for tech’s harms and its role in society.
-          </p>
-          <p>
-            We must do better than technosolutionism—we’re currently growing our
-            organization not just to build short term projects, but engage our
-            members in activism, foster discussions on campus, and promote
-            service in our communities.
-          </p>
-          <p>
-            If this mission toward a better future resonates, we’d love to have
-            you join us.
-          </p>
-          <div className={landingStyles.buttons}>
-            <Link href="/students/">
-              <motion.div
-                whileHover={{
-                  y: 4,
-                  transition: { duration: 0.125 },
-                }}
-                whileTap={{ scale: 0.95 }}
-                className={landingStyles.primarybutton}
-              >
-                Apply now
-              </motion.div>
-            </Link>
+        <div className={landingStyles.photogrid}>
+          <div className={landingStyles.photo}>
+            <Image
+              src={group}
+              alt="All Team Photo of Code the Change"
+              placeholder="blur"
+            />
+          </div>
+          <Image
+            className={landingStyles.photo}
+            src={dinner}
+            alt="Code the Change Dinner Photo"
+            placeholder="blur"
+          />
+          <Image
+            className={landingStyles.photo}
+            src={retreat}
+            alt="Code the Change at Retreat"
+            placeholder="blur"
+          />
+        </div>
+        <div className={landingStyles.mission}>
+          <div>
+            <h5>OUR MISSION</h5>
+            <h3>
+              Design and build sustainable software for nonprofit organizations
+              to create social change.
+            </h3>
           </div>
         </div>
+        <h6>
+          Every year, we partner with several nonprofits in the Los Angeles
+          area. Many nonprofits do not have the resources in the technology
+          department; we build something free-of-charge to help them achieve
+          their mission.
+        </h6>
+        <h6>
+          As an organization, we are structured in project teams, with
+          developers and designers as well as a Tech Lead and Product Manager to
+          lead the team.
+        </h6>
+        <Image src={organization} alt="Alumni Night" placeholder="blur" />
+        <h6>
+          We're committed to our impact, but we go beyond work — 'CTC' is a
+          family. During an Alumni night last year, we had members all the way
+          back from Code the Change's founding year (!) come in and hang out
+          with us. We support each other beyond just our projects.
+        </h6>
+        <h6>
+          Want to learn more? <Link href="/students/">Join us</Link>
+        </h6>
+        <h2>Our Values</h2>
+        <div className={landingStyles.values}>
+          {values.map((value) => {
+            return <Value {...value} />
+          })}
+        </div>
         <Features />
-
         <Footer />
       </div>
     </>
