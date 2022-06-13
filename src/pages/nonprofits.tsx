f2da9222970a31228edaea07863760ec82f33573
Matthew Cho <mcho9434@usc.edu>
2022-06-13 15:01:12 -0400

    Update nonprofits.tsx

--- src/pages/nonprofits.tsx
+++ src/pages/nonprofits.tsx
@@ -65,7 +65,7 @@ export default function Nonprofits() {
               backend system.
             </p>
             <p>
-              Pro bono! We open up applications for Nonprofits in May and select
+              Pro boono! We open up applications for Nonprofits in May and select
               the best fit for their needs and our bandwidth. Apply or send us
               an email with questions!
             </p>
